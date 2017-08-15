d0fc810c0c9a82c6301398e537e695840e9f4e07
Kat <katatk@users.noreply.github.com>
2017-08-15 23:18:56 +1200

    made markers add to map

--- js/map.js
+++ js/map.js
@@ -1,76 +1,78 @@
-var markers = [
-    {
-        name: 'Hombo',
-        lat: -36.889907,
-        lng: 174.763796
+function initMap() {
+
+    var markers = [
+        {
+            name: 'Hombo',
+            lat: -36.889907,
+            lng: 174.763796
     },
-    {
-        name: 'Ramarama',
-        lat: -36.889907,
-        lng: 174.763796
+        {
+            name: 'Ramarama',
+            lat: -36.889907,
+            lng: 174.763796
     },
-    {
-        name: 'Counties Manukau',
-        lat:  -37.041573,
-        lng: 1174.881557
+        {
+            name: 'Counties Manukau',
+            lat: -37.041573,
+            lng: 1174.881557
     },
-    {
-        name: 'Devonport',
-        lat: -36.817135,
-        lng: 174.802538
+        {
+            name: 'Devonport',
+            lat: -36.817135,
+            lng: 174.802538
     },
-    {
-        name: 'Howick',
-        lat: -36.899898, 
-        lng: 174.915544
+        {
+            name: 'Howick',
+            lat: -36.899898,
+            lng: 174.915544
     },
-    {
-        name: 'Henderson',
-        lat: -36.871351,
-        lng: 174.618343
+        {
+            name: 'Henderson',
+            lat: -36.871351,
+            lng: 174.618343
     },
-    {
-        name: 'Waima',
-        lat: -36.946614, 
-        lng: 174.629638
+        {
+            name: 'Waima',
+            lat: -36.946614,
+            lng: 174.629638
     },
-    {
-        name: 'Whangaparaoa',
-        lat: -36.636357, 
-        lng: 174.747909
+        {
+            name: 'Whangaparaoa',
+            lat: -36.636357,
+            lng: 174.747909
     },
-    {
-        name: 'Waitakere',
-        lat: -36.890218, 
-        lng: 174.690345
+        {
+            name: 'Waitakere',
+            lat: -36.890218,
+            lng: 174.690345
     },
-    {
-        name: 'Ramarama',
-        lat: -36.889907,
-        lng: 174.763796
+        {
+            name: 'Ramarama',
+            lat: -36.889907,
+            lng: 174.763796
     },
-    {
-        name: 'Ramarama',
-        lat: -36.889907,
-        lng: 174.763796
+        {
+            name: 'Ramarama',
+            lat: -36.889907,
+            lng: 174.763796
     }
-    
+
 ];
 
-function initMap() {
-    
-  /*  var homboDojo = {
-        lat: -36.889907,
-        lng: 174.763796
-    };*/
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: lat: 40.9006, lng: 174.8860
+        zoom: 5,
+        center: {
+            lat: -41.593481,
+            lng: 174.475167
+        }
     });
 
     for (var i = 0; i <= markers.length; i++) {
         var marker = new google.maps.Marker({
-            position: 'lat: ' + markers[i].lat + ', lng: ' + markers[i].lng,
+            position: {
+                lat: markers[i].lat,
+                lng: markers[i].lng
+            },
             map: map,
             icon: 'http://localhost/aikido/wordpress/wp-content/themes/genesis-sample/images/marker-1.svg'
         });
